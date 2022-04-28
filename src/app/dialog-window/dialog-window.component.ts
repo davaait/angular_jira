37c8c69f1611192fbf21c144524d2fa249bd091b
davidait <david.air@hf-solution.com>
2022-04-28 16:20:49 +0300

    upload file

--- src/app/dialog-window/dialog-window.component.ts
+++ src/app/dialog-window/dialog-window.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 import {TasksControls} from "../model/controls.enum";
 import {Task, TasksStore} from "../services/types";
 import {Collections} from "../services/crud/collections";
 import {CrudService} from "../services/crud/crud.service";
+import {UploadService} from "../services/upload/upload.service";
+import {combineLatest, takeWhile} from "rxjs";
 
 @Component({
   selector: 'app-dialog-window',
@@ -14,13 +16,37 @@ export class DialogWindowComponent implements OnInit {
   public priorities: string[] = ['Low', 'Normal', 'High'];
   public groups: string[] = ['Pending', 'Completed', 'Inprogress', 'Review'];
 
+  public imageLink: string | null = "";
+
+  public progress: string | undefined = "";
+
   public myForm: FormGroup = new FormGroup({});
 
   public data: TasksStore[] = [];
 
   public formControls: typeof TasksControls = TasksControls;
 
-  constructor(private crudService: CrudService) { }
+  constructor(private crudService: CrudService, private uploadService: UploadService) {
+  }
+
+  public onFileSelected(event: Event): void {
+    if (event) {
+      const eventTarget = (<HTMLInputElement>event?.target);
+      if (eventTarget && eventTarget.files) {
+        const file: File = eventTarget.files[0];
+        combineLatest(this.uploadService.uploadFileAndGetMetadata('test', file))
+          .pipe(
+            takeWhile(([, link]) => {
+              return !link;
+            }, true),
+          )
+          .subscribe(([percent, link]) => {
+            this.progress = percent;
+            this.imageLink = link;
+          });
+      }
+    }
+  }
 
   public myFilter = (d: Date | null): boolean => {
     const day = (d || new Date()).getDay();
@@ -32,20 +58,19 @@ export class DialogWindowComponent implements OnInit {
     this.crudService.getDate<TasksStore>(Collections.TASKS).subscribe((value: TasksStore[]) => {
       this.data = value;
     })
-    this.myForm.valueChanges.subscribe(value => console.log(value));
-    this.myForm.addControl(TasksControls.name, new FormControl("", Validators.compose([Validators.required, Validators.minLength(3)])));
-    this.myForm.addControl(TasksControls.priority, new FormControl("", Validators.required));
-    this.myForm.addControl(TasksControls.dueDate, new FormControl("", Validators.required));
-    this.myForm.addControl(TasksControls.group, new FormControl("", Validators.required));
+    this.myForm.addControl(TasksControls.name, new FormControl("", Validators.compose([Validators.required, Validators.maxLength(15)])));
+    // this.myForm.addControl(TasksControls.priority, new FormControl("", Validators.required));
+    // this.myForm.addControl(TasksControls.dueDate, new FormControl("", Validators.required));
+    // this.myForm.addControl(TasksControls.group, new FormControl("", Validators.required));
   }
 
-  public addTask(newTask:any): any {
+  public addTask(newTask: Task): void {
     this.crudService.createObject(Collections.TASKS, newTask).subscribe((value) => console.log(value));
   }
 
   public submitForm(): void {
     if (this.myForm.valid) {
-      const newTask: any = {
+      const newTask: Task = {
         name: this.myForm?.controls[TasksControls.name].value,
         priority: this.myForm?.controls[TasksControls.priority].value,
         dueDate: this.myForm?.controls[TasksControls.dueDate].value,
